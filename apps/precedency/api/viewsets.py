0db6ad371736b7e13820942c6db1121b5ec5d3fb
Vlad Saichenko <saichenkovlad@gmail.com>
2021-09-08 03:53:50 +0700

    Create ViewSets

---
+++ apps/precedency/api/viewsets.py
@@ -0,0 +1,25 @@
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+
+from apps.precedency import models
+from .serializers import PrecedenceSerializer, UsersPrecedencySerializer
+
+
+class PrecedenceViewSet(
+        GenericViewSet,
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+):
+    queryset = models.Precedence.objects.all()
+    serializer_class = PrecedenceSerializer
+
+
+class UsersPrecedencyViewSet(ModelViewSet):
+    queryset = models.UsersPrecedency.objects.all()
+    serializer_class = UsersPrecedencySerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return UsersPrecedency queryset related to current user."""
+        return models.UsersPrecedency.objects.filter(user=self.request.user)
