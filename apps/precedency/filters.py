6f4770975256f3ff85f3cf893942f6d19d776fbf
Vlad Saichenko <saichenkovlad@gmail.com>
2021-09-08 04:52:53 +0700

    Create UsersPrecedencyFilter

--- apps/precedency/filters.py
+++ apps/precedency/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 
-from .models import Precedence
+from .models import Precedence, UsersPrecedency
 
 
 class PrecedenceFilter(django_filters.FilterSet):
@@ -12,4 +12,27 @@ class PrecedenceFilter(django_filters.FilterSet):
         model = Precedence
         fields = (
             "title",
+        )
+
+
+class UsersPrecedencyFilter(django_filters.FilterSet):
+    """UsersPrecedency filterset class."""
+
+    importance__gt = django_filters.NumberFilter(
+        field_name='importance',
+        lookup_expr='gt'
+    )
+    importance__lt = django_filters.NumberFilter(
+        field_name='importance',
+        lookup_expr='lt'
+    )
+
+    class Meta:
+        model = UsersPrecedency
+        fields = (
+            "importance",
+            "importance__gt",
+            "importance__lt",
+            "attitude",
+            "precedency",
         )
